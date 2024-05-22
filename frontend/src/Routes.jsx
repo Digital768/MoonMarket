4c9872d40780ae76ce5876fcb4fc9f35654d79eb
Digital768 <121376732+Digital768@users.noreply.github.com>
2024-05-22 16:43:17 +0300

    added skeleton for the stock image loading

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,70 +1,69 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { useAuth } from "@/pages/AuthProvider";
 import { ProtectedRoute } from "@/pages/ProtectedRoute";
-import App, {action as appAction} from '@/pages/App';
-import ErrorPage from '@/pages/ErrorPage';
-import StockPage from '@/pages/StockPage';
-import StockItem from '@/pages/StockItem';
+import App, { action as appAction } from "@/pages/App";
+import ErrorPage from "@/pages/ErrorPage";
+import StockPage from "@/pages/StockPage";
+import StockItem from "@/pages/StockItem";
 import Login from "@/pages/Login";
 import Logout from "@/pages/Logout";
-import {getUserData} from '@/api/user'
-import {getStockFromPortfolio, getStockData} from '@/api/stock'
-import {PublicRoute} from '@/pages/PublicRoute'
+import { getUserData } from "@/api/user";
+import { getStockFromPortfolio, getStockData } from "@/api/stock";
+import { PublicRoute } from "@/pages/PublicRoute";
 
 const Routes = () => {
   const { token } = useAuth();
-
 
   // Combine and conditionally include routes based on authentication status
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <ProtectedRoute/>,
+      element: <ProtectedRoute />,
       children: [
         {
-          path: '/portfolio',
-          element: <App /> ,
-          errorElement: <ErrorPage/>,
+          path: "/portfolio",
+          element: <App />,
+          errorElement: <ErrorPage />,
           loader: async () => {
-            console.log("loader activated")
-            const user = await getUserData(token)
+            console.log("loader activated");
+            const user = await getUserData(token);
             // console.log("user: " , user.data)
-            return user
-          }, 
-         action:appAction
+            return user;
+          },
+          action: appAction,
         },
         {
           path: "portfolio/:stockTicker",
           element: <StockPage />,
-          errorElement: <ErrorPage/>,
-          loader: ({params}) => {
-            return getStockFromPortfolio(params.stockTicker, token)
-          }
+          errorElement: <ErrorPage />,
+          loader: ({ params }) => {
+            return getStockFromPortfolio(params.stockTicker, token);
+          },
         },
         {
           path: "/logout",
-          element: <Logout/>,
+          element: <Logout />,
         },
         {
           path: "stock/:stockTicker",
-          element: <StockItem/>,
-          errorElement: <ErrorPage/>,
-          loader: ({params}) => {
-            return getStockData(params.stockTicker, token)
-          }
-        }
-      ]
+          element: <StockItem />,
+          errorElement: <ErrorPage />,
+          loader: ({ params }) => {
+            return getStockData(params.stockTicker, token);
+          },
+        },
+      ],
     },
     {
-      path: '/login',
+      path: "/login",
       element: <PublicRoute />,
       children: [
         {
-          path: '/login',
-          element: <Login />
-        }
-      ]
-    }
+          path: "/login",
+          element: <Login />,
+        },
+      ],
+    },
   ]);
 
   // Provide the router configuration using RouterProvider
@@ -72,6 +71,3 @@ const Routes = () => {
 };
 
 export default Routes;
-
-
-
